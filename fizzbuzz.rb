2f573979a5f6d43dc9f8ec7512268dff5a4a127d
Irum F <irummf@gmail.com>
2018-08-24 00:05:23 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,11 @@
-def fizzbuzz(num)
-  if num % 3 == 0
-  print "Fizz"
-end
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0 
+  "FizzBuzz"
+  elsif int % 5 == 0 
+  "Buzz"
+  elsif int % 3 == 0
+  "Fizz"
+  else 
+    nil 
+  end
 end
